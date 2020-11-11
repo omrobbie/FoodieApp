26659e2d417a1ee46f75367b6c40c6d93937da07
omrobbie <putu.roby@yahoo.com>
2020-11-11 11:13:32 +0800

    * create new foodie app for iOS *

---
+++ FoodieApp-iOS/FoodieApp-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodieApp-iOS
+//
+//  Created by omrobbie on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
