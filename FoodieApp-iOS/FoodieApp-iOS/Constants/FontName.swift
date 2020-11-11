f79da3236e72d378b2c2847043fe1d3f64886ae9
omrobbie <putu.roby@yahoo.com>
2020-11-11 12:19:58 +0800

    import Poppins fonts

---
+++ FoodieApp-iOS/FoodieApp-iOS/Constants/FontName.swift
@@ -0,0 +1,14 @@
+//
+//  FontName.swift
+//  FoodieApp-iOS
+//
+//  Created by omrobbie on 11/11/20.
+//
+
+struct FontPoppins {
+    static let light = "Poppins-Light"
+    static let regular = "Poppins-Regular"
+    static let medium = "Poppins-Medium"
+    static let semiBold = "Poppins-SemiBold"
+    static let bold = "Poppins-Bold"
+}
