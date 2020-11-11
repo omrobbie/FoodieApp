f23bafe4934a83e05f2801e13f2b73a2bf57e807
omrobbie <putu.roby@yahoo.com>
2020-11-11 16:07:40 +0800

    update SwiftLint run script and fix the warnings

--- FoodieApp-iOS/FoodieApp-iOS/App/SceneDelegate.swift
+++ FoodieApp-iOS/FoodieApp-iOS/App/SceneDelegate.swift
@@ -11,7 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 }
