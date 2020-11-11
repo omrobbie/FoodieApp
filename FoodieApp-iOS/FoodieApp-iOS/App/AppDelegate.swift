cf411f195e6c3eca5857e27e25d9975110bbaf54
omrobbie <putu.roby@yahoo.com>
2020-11-11 11:30:33 +0800

    clean project with clean architecture folder structure

---
+++ FoodieApp-iOS/FoodieApp-iOS/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FoodieApp-iOS
+//
+//  Created by omrobbie on 11/11/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
