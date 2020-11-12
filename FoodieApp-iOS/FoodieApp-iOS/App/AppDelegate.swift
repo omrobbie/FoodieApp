a50f74d0a08f16755403f7793a3a36944d70b7ad
omrobbie <putu.roby@yahoo.com>
2020-11-12 16:31:57 +0800

    import IQKeyboardManager to handle keyboard observer

--- FoodieApp-iOS/FoodieApp-iOS/App/AppDelegate.swift
+++ FoodieApp-iOS/FoodieApp-iOS/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         return true
     }
 }
